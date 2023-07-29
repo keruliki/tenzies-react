80f72bd2fa4c50738971498c96b1b59829b0fc88
Khairul Fikri <48195138+keruliki@users.noreply.github.com>
2023-07-29 22:29:43 +0800

    update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Die from "./Die";
+import { nanoid } from "nanoid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <main>
+        <div>
+          <h1 className="title">Yahtzee!</h1>
+        </div>
+        <div className="dice-container">{diceElements}</div>
+        <button className="roll-dice" onClick={rollDice}>
+          Roll
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+          <p>By keruliki</p>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Die.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "green" : "white",
+  };
+  return (
+    <div className="die-face" style={styles} onClick={props.holdDice}>
+      <h2 className="die-num">{props.value}</h2>
+    </div>
+  );
+}
+
+export default Die;
