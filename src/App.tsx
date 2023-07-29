356c0b65f7ce3365b71be8633077ec39bc5faa7f
Khairul Fikri <48195138+keruliki@users.noreply.github.com>
2023-07-29 22:40:41 +0800

    complete project and start new game

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Die from "./Die";
 import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      alert("TENZIES!");
+    } else {
+      setTenzies(false);
+    }
+  }, [dice]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+  }
+
+  function newGame(){
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? generateNewDie() : die;
+      })
+    );
+    setTenzies(false);
   }
 
   function holdDice(id) {
@@ -46,9 +77,15 @@ function App() {
           <h1 className="title">Yahtzee!</h1>
         </div>
         <div className="dice-container">{diceElements}</div>
-        <button className="roll-dice" onClick={rollDice}>
+        {!tenzies ? (
+          <button className="roll-dice" onClick={rollDice}>
           Roll
+        </button>) : (
+          <button className="roll-dice" onClick={newGame}>
+          New Game
         </button>
+        )  
+        }
         <div>
           <p>By keruliki</p>
         </div>
